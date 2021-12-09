bbdd4224593102d5ceaa9ee00044338f99dab685
Hojun_Yoo <lisiee0@naver.com>
2021-12-09 16:37:49 +0900

    ex06

--- src/com/javaex/ex06/Person.java
+++ src/com/javaex/ex06/Person.java
@@ -1,5 +1,53 @@
 package com.javaex.ex06;
 
 public class Person {
+	private String name;
+	private String hp;
+	private String company;
+	
+	public Person() {
+		
+	}
 
+	public Person(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	public void showInfo() {
+		System.out.println("이름: "+name);
+		System.out.println("핸드폰: "+hp);
+		System.out.println("회사: "+company);
+		System.out.println("");
+	}
+	
+	public String save() {
+		String save= name+","+hp+","+company;
+		return save;
+	}
 }
--- src/com/javaex/ex06/PhoneApp.java
+++ src/com/javaex/ex06/PhoneApp.java
@@ -1,10 +1,64 @@
 package com.javaex.ex06;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PhoneApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		Reader rd= new FileReader("C:\\javaStudy\\file\\PhoneDB.txt");
+		BufferedReader br= new BufferedReader(rd);
+		
+		List<Person> pList= new ArrayList<Person>();
+		String line, name, hp, company, info;
+		
+		while(true) {
+			line= br.readLine();
+			if(line==null) {
+				break;
+			}
+			name= line.split(",")[0];
+			hp= line.split(",")[1];
+			company= line.split(",")[2];
+			
+			Person np= new Person(name, hp, company);
+			pList.add(np);
+		}
+		
+		for(Person p: pList) {
+			p.showInfo();
+		}
+		
+		// 여기까지 txt파일 읽고 List에 저장하기
+		
+		// 아래부터 데이터 추가후 다시 출력하기
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("=======================================");
+		System.out.println("데이터를 입력하세요.");
+		
+		
+		info= sc.nextLine();
+		name= info.split(",")[0];
+		hp= info.split(",")[1];
+		company= info.split(",")[2];
+		Person np01= new Person(name, hp, company);
+		pList.add(np01);
+		
+		System.out.println("=======================================");
+		
+		for(Person p: pList) {
+			p.showInfo();
+		}
+
+		sc.close();		
+		br.close();
 
 	}
-
 }
