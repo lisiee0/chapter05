541100e884c40eecfb4fa7d7572d22c373a7193d
Hojun_Yoo <lisiee0@naver.com>
2021-12-09 16:16:51 +0900

    12-09

--- src/com/javaex/ex05/Person.java
+++ src/com/javaex/ex05/Person.java
@@ -5,10 +5,37 @@ public class Person {
 	private String hp;
 	private String company;
 	
+	public Person() {
+		
+	}
 	
 	public Person(String name, String hp, String company) {
 		this.name = name;
 		this.hp = hp;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
 		this.company = company;
 	}
 
